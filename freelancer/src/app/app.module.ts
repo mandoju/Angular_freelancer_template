38b3f2489873532dbbb924307d7092ed77a02631
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-05-29 03:21:27 -0300

    adicionado template básico
    Template básico funcionado.
    
    @todo: funcionalidade para o header crescer no topo da página
    @todo: funcionalidades mobile
    @todo: backend para adição e remoção de informações

--- freelancer/src/app/app.module.ts
+++ freelancer/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule
